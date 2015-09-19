aef7e09f68b2e304a2a844f88cb22633eece4524
max-silent <mirotvoretz@gmail.com>
2015-09-19 20:25:50 +0300

    Create Project13b.ino (Touchy-feely lamp)
    Touchy-feely lamp (another version).
    
    Switching On/Off the LED via the Capacitive sensor (based on the third party CapacitiveSensor library by Paul Badger. The library should be installed into the Arduino IDE).

---
+++ Project13b.ino
@@ -0,0 +1,35 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor capSensor = CapacitiveSensor(4,2);
+int treshold=700;
+const int ledPin=8;
+unsigned long lastOutputMoment=0;
+boolean touchedWire=false;
+boolean turnedOnLed=false;
+
+void setup()                    
+{
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()                    
+{
+  unsigned long start = millis();
+  long sensorValue = capSensor.capacitiveSensor(50);
+  if (sensorValue>treshold){
+    if (!touchedWire){
+      touchedWire=true;
+      digitalWrite(ledPin,turnedOnLed?LOW:HIGH);
+      turnedOnLed=!turnedOnLed;
+    }
+  }else touchedWire=false;
+  if (millis() - lastOutputMoment>250){
+  Serial.print(millis() - start);        // check on performance in milliseconds
+  Serial.print("\t");                    // tab character for debug windown spacing
+  Serial.print(sensorValue);             // print sensor output 1
+  Serial.println("");
+  lastOutputMoment=millis();
+  }
+  delay(1);                             // arbitrary delay to limit data to serial port 
+}
